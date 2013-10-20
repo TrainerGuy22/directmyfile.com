6760925a72fbfe8c1b7989001731dbdc635f1e29
Kenneth Endfinger <kaendfinger@gmail.com>
2013-10-21 03:44:37 +0400

    Additions, bug fixes and more.

--- js/site.js
+++ js/site.js
@@ -1,7 +1,21 @@
-$.getScript('/js/bootbox.js');
+WebFont.load({
+    google: {
+      families: ['Finger Paint', 'Open Sans']
+    }
+});
+
+function sendN(email) {
+  $.get("http://n.tkte.ch/h/1571/4RRGZ_8mWPPgIcNMwQinEYu0?payload=" + encodeURIComponent("> " + email + " asked to be contacted."), function(d) {
+    bootbox.alert(d);
+  });
+}
 
 $('#contactUs').click(function () {
   bootbox.prompt('Enter your Email', function (email) {
-    console.log('Email was inputted: ' + email);
+    if(email==null || email=='') {
+      return;
+    }
+    sendN(email);
+    bootbox.alert("Request Sent");
   });
 });
